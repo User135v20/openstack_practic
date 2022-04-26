22abc5be605a3a1a91f9585e53409b70cb940e80
Roman <gra432322@gmail.com>
2022-04-26 22:24:37 +0300

    create server

---
+++ main.py
@@ -0,0 +1,58 @@
+from glanceclient.client import Client as _glance
+from keystoneauth1 import identity, session
+from neutronclient.v2_0 import client
+from novaclient.client import Client as _nova
+
+
+def create_network(sess):
+    neutron = client.Client(session=sess)
+    print(neutron.list_networks())
+
+    body_sample = {"network": {"name": "test"}}
+    netw = neutron.create_network(body=body_sample)
+    net_dict = netw["network"]
+    network_id = net_dict["id"]
+    print("Network %s created" % network_id)
+
+    body_create_subnet = {"subnets": [{"cidr": "192.168.199.0/24", "ip_version": 4, "network_id": network_id}]}
+    subnet = neutron.create_subnet(body=body_create_subnet)
+    print("Created subnet %s" % subnet)
+    subnet_id = subnet.get("id")
+    return network_id, subnet_id
+
+
+def create_session():
+    username = "demo"
+    password = "secret"
+    project_name = "demo"
+    project_domain_id = "default"
+    user_domain_id = "default"
+    auth_url = "http://10.0.2.15/identity"
+    auth = identity.Password(
+        auth_url=auth_url,
+        username=username,
+        password=password,
+        project_name=project_name,
+        project_domain_id=project_domain_id,
+        user_domain_id=user_domain_id,
+    )
+    sess = session.Session(auth=auth)
+    return sess
+
+
+def create_server(sess):
+    nova = _nova("2", session=sess)
+    flavor_id = "c1"
+    flavor = nova.flavors.get(flavor_id)
+    image_id = "e207ec98-21d4-47d7-b491-384ca824631f"
+
+    glance = _glance("2", session=sess)
+    image = glance.images.get(image_id)
+    server = nova.servers.create("TestServer2", image, flavor, nics=[{"net-id": network_id}])
+    print(server.id)
+    return server
+
+
+sess = create_session()
+network_id, subnet_id = create_network(sess)
+server = create_server(sess)
